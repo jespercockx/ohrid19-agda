5d988c952fe7b9a030e1d5b8a7aa597c59cbadc8
Jesper Cockx <jesper@sikanda.be>
2019-08-28 11:49:01 +0200

    Two more test cases

---
+++ test/simple.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#define printInt(e) printf("%d\n",e)
+
+int main () {
+  printInt(1+1);
+}
---
+++ test/variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define printInt(e) printf("%d\n",e)
+
+int main () {
+  int a = 1;
+  int b = 2;
+  int c = 3;
+  printInt(a+b+c);
+}
